ba9c3658aacf20783a58c04c38969af3ad3e7171
fFukurou <supevi1@gmail.com>
2024-12-13 16:07:15 -0300

    added first enemy

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -6,6 +6,8 @@ func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
 	timer.start()
 	
-	
-func _on_timer_timeout():
+
+
+
+func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
