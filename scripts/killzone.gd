2941c111f4cc512362df668758be546b31b7aca7
fFukurou <supevi1@gmail.com>
2024-12-13 21:22:15 -0300

    added SFX, score, death animation...

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,29 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var sfx: AudioStreamPlayer2D = $SFX1
+@onready var sfx_2: AudioStreamPlayer2D = $SFX2
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	randomize()
 
 func _on_body_entered(body: Node2D) -> void:
+	var random_number = randi_range(0, 1)
 	print("You died!")
+	Engine.time_scale = 0.5
+	body.dead = true
+	body.get_node("CollisionShape2D").queue_free()
+	if random_number == 0:
+		sfx.play()
+	else:
+		sfx_2.play()
 	timer.start()
 	
 
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var dead = false
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +14,34 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+		
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction and handle the movement/deceleration. (-1, 0, 1)
+	var direction := Input.get_axis("move_left", "move_right") 	
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif  direction < 0:
+		animated_sprite.flip_h = true
+		
+		
+	# Play animations
+	if dead == true:
+		animated_sprite.play("dead")
+	elif is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("running")
+	else:
+		animated_sprite.play("jumping")
+	
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
